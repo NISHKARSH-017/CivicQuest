49036113edb4542da2da6e4c8452696df3f5a18b
swatimedhi4-dotcom <swatimedhi4@gmail.com>
2026-09-13 03:40:04 +0530

    Update location name in homePage.js

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -18,7 +18,7 @@ export function renderHomePage(container) {
       <!-- 1. Welcome & Primary Action Banner -->
       <section class="welcome dashboard-welcome">
         <div>
-          <small class="today-date">TODAY IN MAPLEWOOD, PORTLAND</small>
+          <small class="today-date">TODAY IN VELLORE, TAMIL NADU</small>
           <h1>Hello! ${state.user.name.split(' ')[0]} <em>✦</em></h1>
           <p>You are a <strong class="badge-highlight">${state.user.badge}</strong>. Small local actions make a big neighborhood difference.</p>
         </div>
